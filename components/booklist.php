db3c16c45a39a7b895f3e666e284d2899a84f104
Aayush Solanki <aayushsolanki40@gmail.com>
2022-10-15 21:54:01 +0530

    added home page

---
+++ components/booklist.php
@@ -0,0 +1,8 @@
+<div class="card boolist_card">
+<img class="card-img-top" src="assets/images/books-sample.jpg" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">Mahashatra</h5>
+        <p class="card-text">Some quick example book to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-success">Buy Now</a>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include_once('layouts/user/header.php'); ?>
+<?php include_once('layouts/user/navbar.php'); ?>
+<?php
+    $variable = [
+        "", "", "", "", "", "", "", "", "" ,""
+    ]
+?>
+<div class="container main_middle_content">
+    <div class="row">
+        <?php
+        foreach ($variable as $key => $value) { ?>
+            <div class="col-md-3 booklist_col">
+                <?php include('components/booklist.php'); ?>
+            </div>
+        <?php } ?>
+    </div>
+</div>
+<?php include_once('layouts/user/footer.php'); ?>
